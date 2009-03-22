126655ac41c012bb66374e1a5136dff577dbf90c
jan <jan@d6a85330-1cd7-4390-b269-eb986cef6744>
2009-03-22 18:09:23 +0000

    doku
    git-svn-id: file:///home/jan/tmp/HTW/trunk@239 d6a85330-1cd7-4390-b269-eb986cef6744

--- program.pl
+++ program.pl
@@ -3,7 +3,7 @@ r(l).
 r(r).
 r(n).
 
-% b(zeichen) .. Eingabealphabet
+% e(zeichen) .. Eingabealphabet
 e('1').
 e('0').
 
@@ -31,10 +31,10 @@ za(z(0)).
 % % ist blank
 % -1 ist ende
 % 0 ist start
-% bewegen: 0->stehen, 1->next, -1->prev
-zu(z(0),'%',  '1',  r(l), z(1)).
-zu(z(1),'%',  '1',  r(l), z(2)).
-zu(z(2),'%',  '1',  r(n), z(3)).
+% bewegen: n->stehen, r->right, l->left
+zu(z(0),'%', '1',  r(l), z(1)).
+zu(z(1),'%', '1',  r(l), z(2)).
+zu(z(2),'%', '1',  r(n), z(3)).
 zu(z(3),'1', '1',  r(r), z(3)).
 zu(z(3),'%', '%',  r(l), z(4)).
 
--- turing.pl
+++ turing.pl
@@ -10,7 +10,7 @@ myPop([], Elem, Result) :- bl(X), Elem = X, Result = '[]'.
 % Vorheriges Bandelement
 myPrev(PListOld, PListNew, NListOld, NListNew, ElemOld, ElemNew) :- myPush(NListOld, ElemOld, NListNew), myPop(PListOld, ElemNew, PListNew).
 
-% Nästes Bandelement
+% Naestes Bandelement
 myNext(PListOld, PListNew, NListOld, NListNew, ElemOld, ElemNew) :- myPush(PListOld, ElemOld, PListNew), myPop(NListOld, ElemNew, NListNew).
 
 % Bewegung des Bandes
@@ -29,16 +29,18 @@ myPrintIter([],    NList,  Elem)  :- write('['), write(Elem), write(']'), myPrin
 myPrintIter([X|Y], NList,  Elem)  :- write(X), write(', '), myPrintIter(Y, NList, Elem),!.
 
 % Ausgabe eines Bandes
-myPrint(PList, NList,  Elem) :- bl(X), write('..., '), write(X), write(' , '), write(X), write(', '), 
-                                reverse(PList,PList1),
-                                myPrintIter(PList1, NList, Elem), 
-				write(', '), write(X), write(', '), write(X), write(', ...').
-myPrint2(PList, NList,  Elem) :- replaceNonInput(PList, P), 
-                                 replaceNonInput([Elem|NList], [Z|N]), 
-				 reverse(P,P1),
-				 write('['),
-				 myPrintIter(P1,N,Z),
-				 write(']').
+myPrint(PList, NList,  Elem) :- 
+        bl(X), write('..., '), write(X), write(' , '), write(X), write(', '), 
+        reverse(PList,PList1),
+        myPrintIter(PList1, NList, Elem), 
+	write(', '), write(X), write(', '), write(X), write(', ...').
+myPrint2(PList, NList,  Elem) :- 
+        replaceNonInput(PList, P), 
+        replaceNonInput([Elem|NList], [Z|N]), 
+	reverse(P,P1),
+	write('['),
+	myPrintIter(P1,N,Z),
+	write(']').
 
 % Ausgabe der momentanen Bewegungsrichtung.
 myPrintDir(r(n)) :- write('Nicht ').
@@ -47,43 +49,48 @@ myPrintDir(r(r)) :- write('Rechts').
 
 % Ausgabe des Fortschrittes der Turingmaschine
 myPrintProgress(PList, NewPList, NList, NewNList,  Elem, NewElem, NewElem2, Zst, NewZst, r(Dir),N)  :- 
-                                                  write('Schritt '),write(N), write(':'),nl,
-                                                  write('    Momentaner Zustand: '), write(Zst), 
-                                                  write('; Momentanes Band:'),nl,
-						  write('      '),myPrint(PList, NList,  Elem),nl,
-                                                  write('    Gelesen: '), write(Elem),
-						  write('; Schreibe: '), write(NewElem),nl,
-						  write('    Bewege: '), myPrintDir(r(Dir)),
-						  write('; Folgezustand: '), write(NewZst),nl,
-						  write('    Resultierendes Band: '), nl, 
-						  write('      '),myPrint(NewPList, NewNList,  NewElem2),nl,
-						  sleep(1),!.
+        write('Schritt '),write(N), write(':'),nl,
+        write('    Momentaner Zustand: '), write(Zst), 
+        write('; Momentanes Band:'),nl,
+	write('      '),myPrint(PList, NList,  Elem),nl,
+        write('    Gelesen: '), write(Elem),
+	write('; Schreibe: '), write(NewElem),nl,
+	write('    Bewege: '), myPrintDir(r(Dir)),
+	write('; Folgezustand: '), write(NewZst),nl,
+	write('    Resultierendes Band: '), nl, 
+	write('      '),myPrint(NewPList, NewNList,  NewElem2),nl,
+	write('fortsetzen mit [Enter] '),
+	get_byte(O),!.
 
 % Vollziehen der Zustandsuebergaenge der Turingmaschine 
-run(PList, NList, Elem, z(Zst), N)  :- ze(z(Zst)), 
-                                       write('Fertig nach '), write(N), write(' Schritten:'), nl,
-                                       write('    Endzustand: '), write(Zst), nl,
-				       write('    Elemente vor der aktuellen Position:  '), length(PList, J), write(J), nl,
-				       write('    Elemente nach der aktuellen Position: '), length(NList, I), write(I), nl,
-				       write('    Resultierendes Band:'),nl, 
-				       write('       '), myPrint(PList, NList,  Elem),nl, 
-				       write('    Ausgabeband (Band ohne Nichteingabesymbole):'),nl, 
-				       write('       '),myPrint2(PList, NList,  Elem),nl. 
-run(PList, NList, Elem, z(Zst), N)  :- b(Elem), 
-                                       zu(z(Zst), Elem, NewElem, r(Dir), z(NewZst)), 
-                                       myMove(r(Dir), PList, NewPList, NList, NewNList, NewElem, NewElem2),
-	                               M is N+1,
-				       myPrintProgress(PList, NewPList, NList, NewNList, Elem, NewElem, NewElem2,Zst,NewZst, r(Dir), M),nl,
-				       run(NewPList, NewNList, NewElem2, z(NewZst), M),!. 
+run(PList, NList, Elem, z(Zst), N)  :- 
+        ze(z(Zst)), 
+        write('Fertig nach '), write(N), write(' Schritten:'), nl,
+        write('    Endzustand: '), write(Zst), nl,
+        write('    Elemente vor der aktuellen Position:  '), length(PList, J), write(J), nl,
+        write('    Elemente nach der aktuellen Position: '), length(NList, I), write(I), nl,
+        write('    Resultierendes Band:'),nl, 
+        write('       '), myPrint(PList, NList,  Elem),nl, 
+        write('    Ausgabeband (Band ohne Nichteingabesymbole):'),nl, 
+        write('       '),myPrint2(PList, NList,  Elem),nl. 
+run(PList, NList, Elem, z(Zst), N)  :- 
+        b(Elem), 
+        zu(z(Zst), Elem, NewElem, r(Dir), z(NewZst)), 
+        myMove(r(Dir), PList, NewPList, NList, NewNList, NewElem, NewElem2),
+        M is N+1,
+	myPrintProgress(PList, NewPList, NList, NewNList, Elem, NewElem, NewElem2,Zst,NewZst, r(Dir), M),nl,
+	run(NewPList, NewNList, NewElem2, z(NewZst), M),!. 
 
-% prepareBand2 bringt die als Startband angegebene Liste in die Form [vorgängerliste], aktuelles 
+% prepareBand2 bringt die als Startband angegebene Liste in die Form [vorgaengerliste], aktuelles 
 % Element, [Nachfolgerliste]. Als Aktuelles Element wird das Element an der als Startposition
 % angegebenen Stelle gesetzt
-prepareBand2(PList, NewPList, NList, NewNList, Elem, NewElem, N) :- N > 0,
-                                                                   myMove(r(l), PList, NewPListTmp, NList, NewNListTmp, Elem, NewElemTmp), 
-                                                                   K is N - 1, 
-								   prepareBand2(NewPListTmp, NewPList, NewNListTmp, NewNList, NewElemTmp, NewElem, K).
-prepareBand2(PList, NewPList, NList, NewNList, Elem, NewElem, 0) :- NewPList = PList, NewNList = NList, NewElem = Elem,!.
+prepareBand2(PList, NewPList, NList, NewNList, Elem, NewElem, N) :- 
+        N > 0,
+        myMove(r(l), PList, NewPListTmp, NList, NewNListTmp, Elem, NewElemTmp), 
+        K is N - 1, 
+	prepareBand2(NewPListTmp, NewPList, NewNListTmp, NewNList, NewElemTmp, NewElem, K).
+prepareBand2(PList, NewPList, NList, NewNList, Elem, NewElem, 0) :- 
+        NewPList = PList, NewNList = NList, NewElem = Elem,!.
 
 % prepareBand ist eine Holfsklausel zum Aufruf von prepareBand2.
 prepareBand(APos, [Z|ABand], PList,NList,Elem) :- prepareBand2([], PList, ABand, NList, Z, Elem, APos).
